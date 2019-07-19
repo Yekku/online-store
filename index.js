02ccc3409ddd04e5bc04fd48426c5e7e635ef380
Yekku <yevgen.liukkonen@gmail.com>
2019-07-19 20:31:42 +0300

    added express-handlebars

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const path = require('path')
+const exhbs = require('express-handlebars')
 
 const app = express()
+const hbs = exhbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
 const PORT = process.env.PORT || 3000
 
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+  res.render('index')
 })
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+  res.render('about')
 })
 
 app.listen(PORT, () => {
