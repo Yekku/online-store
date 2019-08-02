f3318efd1a11c37a9b244f5208d5940e19507d98
Yekku <yevgen.liukkonen@gmail.com>
2019-08-02 18:08:47 +0300

    adde card

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const exhbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
 const coursesRoutes = require('./routes/courses')
 const addRoutes = require('./routes/add')
+const cardRoutes = require('./routes/card')
 
 const app = express()
 const hbs = exhbs.create({
@@ -21,6 +22,7 @@ app.use(express.urlencoded({extended: true}))
 app.use('/', homeRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/add', addRoutes)
+app.use('/card', cardRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
