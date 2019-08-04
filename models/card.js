dc78d158554acf7a6c51447b93d8edee5047e142
Yekku <yevgen.liukkonen@gmail.com>
2019-08-04 22:28:43 +0300

    first release working app

--- models/card.js
+++ models/card.js
@@ -8,47 +8,68 @@ const p = path.join(
 )
 
 class Card {
-  constructor () {
-
-  }
-
   static async add(course) {
-    const card = await Card.fetch()
-    const idx = card.courses.findIndex(c => c.id === course.id)
-    const condidate = card.courses[idx]
+    const card = await Card.fetch();
+    const idx = card.courses.findIndex(c => c.id === course.id);
+    const condidate = card.courses[idx];
     if (condidate) {
       // Course already in card
-      condidate.count++
-      card.courses[idx] = condidate
+      condidate.count++;
+      card.courses[idx] = condidate;
     } else {
       // Course need add
-      course.count = 1
-      card.courses.push(course)
+      course.count = 1;
+      card.courses.push(course);
     }
-    card.price += +course.price
+    card.price += +course.price;
     return new Promise((resolve, reject) => {
       fs.writeFile(p, JSON.stringify(card), err => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve();
-          }
-        },
-      );
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
     });
   }
 
   static async fetch() {
     return new Promise((resolve, reject) => {
       fs.readFile(p, 'utf-8', (err, content) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(JSON.parse(content))
-          }
-        },
-      );
-    })
+        if (err) {
+          reject(err);
+        } else {
+          resolve(JSON.parse(content));
+        }
+      });
+    });
+  }
+
+  static async remove(id) {
+    const card = await Card.fetch();
+
+    const idx = card.courses.findIndex(c => c.id === id);
+    const course = card.courses[idx];
+
+    if (course.count === 1) {
+      // delete
+      card.courses = card.courses.filter(c => c.id !== id);
+    } else {
+      // edit count
+      card.courses[idx].count--;
+    }
+
+    card.price -= course.price;
+
+    return new Promise((resolve, reject) => {
+      fs.writeFile(p, JSON.stringify(card), err => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(card);
+        }
+      });
+    });
   }
 }
 module.exports = Card 
